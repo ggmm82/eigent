42215ee5c19fd2dc37da3c1657cb4fe359a18c1d
sw3205933776 <3205933776@qq.com>
2025-08-20 11:16:14 +0800

    Rename nodeVersion variable

--- config/notarize.cjs
+++ config/notarize.cjs
@@ -8,17 +8,17 @@ exports.default = async function notarizing(context) {
 	const appOutDir = context.appOutDir;
 	const appName = context.packager.appInfo.productName;
 	console.log("appOutDir", appOutDir);
-	console.log("process.env.APPLEID", process.env.APPLEID);
-	console.log("process.env.APPLEIDPASS", process.env.APPLEIDPASS);
-	console.log("process.env.APPLETEAMID", process.env.APPLETEAMID);
+	console.log("process.env.APPLEID", process.env.APPLE_ID);
+	console.log("process.env.APPLEIDPASS", process.env.APPLE_APP_SPECIFIC_PASSWORD);
+	console.log("process.env.APPLETEAMID", process.env.APPLE_TEAM_ID);
 	return notarize({
 		tool: "notarytool",
-		teamId: process.env.APPLETEAMID,
+		teamId: process.env.APPLE_TEAM_ID,
 		appBundleId: "com.eigent.app",
 		appPath: `${appOutDir}/${appName}.app`,
-		appleId: process.env.APPLEID,
-		appleIdPassword: process.env.APPLEIDPASS,
-		ascProvider: process.env.APPLETEAMID,
+		appleId: process.env.APPLE_ID,
+		appleIdPassword: process.env.APPLE_APP_SPECIFIC_PASSWORD,
+		ascProvider: process.env.APPLE_TEAM_ID,
 	})
 		.then((res) => {
 			console.log("success!");
