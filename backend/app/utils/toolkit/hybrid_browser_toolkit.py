6bc7053249b19af3e82472136a941d54a55cf64e
puzhen <1303385763@qq.com>
2025-08-23 16:33:29 +0200

    update

--- backend/app/utils/toolkit/hybrid_browser_toolkit.py
+++ backend/app/utils/toolkit/hybrid_browser_toolkit.py
@@ -15,6 +15,8 @@ from camel.toolkits.hybrid_browser_toolkit.hybrid_browser_toolkit_ts import (
 from camel.toolkits.hybrid_browser_toolkit.ws_wrapper import \
     WebSocketBrowserWrapper as BaseWebSocketBrowserWrapper
 from app.component.command import bun, uv
+from app.component.nodejs_wrapper import get_node_command, run_node_with_nodejs_wheel
+from app.component.environment import env
 from app.service.task import Agents
 from app.utils.listen.toolkit_listen import listen_toolkit
 from app.utils.toolkit.abstract_toolkit import AbstractToolkit
@@ -428,7 +430,7 @@ class HybridBrowserToolkit(BaseHybridBrowserToolkit, AbstractToolkit):
             dom_content_loaded_timeout=self._dom_content_loaded_timeout,
             viewport_limit=self._viewport_limit,
             connect_over_cdp=self.config_loader.get_browser_config().connect_over_cdp,
-            cdp_url=self.config_loader.get_browser_config().cdp_url,
+            cdp_url=f"http://localhost:{env('browser_port', '9222')}",
         )
 
     @classmethod
