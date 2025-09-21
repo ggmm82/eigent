0d0a7472f366ea5eedc60de6ed66826859735582
ggmm82 <marco.gramuglia@gmail.com>
2025-09-21 17:01:03 +0200

    Update start.sh

--- server/start.sh
+++ server/start.sh
@@ -2,7 +2,7 @@
 
 # wait for database to be ready
 echo "Waiting for database to be ready..."
-while ! nc -z postgres 5432; do
+while ! nc -z library-postgres-ov0ysr.internal 5432; do
   sleep 1
 done
 echo "Database is ready!"
